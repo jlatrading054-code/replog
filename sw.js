aed6d396f290c581e614e05045dadd6f21c60be3
jlatrading054-code <jlatrading054@gmail.com>
2026-05-15 22:41:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'replog-v1';
+const CACHE = 'replog-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
